8e7e554ecaacf3f34f202d3e56d8a6a7105d7f71
dani3lm3na1999 <dani3lm3na@gmail.com>
2022-02-25 19:31:51 -0600

    Create Server

---
+++ server.js
@@ -0,0 +1,13 @@
+//install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/inventario'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/inventario/'}),
+);
+
+app.listen(process.env.PORT || 4800);
